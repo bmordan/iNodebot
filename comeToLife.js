0d6f493f7da2c32231da61b2e3369f938c5061ee
Bernard Mordan <bernardmordan@gmail.com>
2014-12-23 23:20:42 +0000

    basic sockets hooked up

--- comeToLife.js
+++ comeToLife.js
@@ -1,17 +1,13 @@
 var five = require('johnny-five');
 var io   = require('socket.io')();
 var Piezo = require('./Piezo');
-var socket = require('socket.io-client')('http://192.168.0.13');
+var socket = require('socket.io-client')('http://192.168.0.13:3000');
 var board = new five.Board();
 
-
 board.on('ready',function(){
-  
   var piezo = new Piezo(five);
+
   piezo.awaken();
 
-  socket.on('connect', function(){
-    console.log('THREE LAWS SAFE iNodebot at your service')
-  })
-
+  socket.emit('ready', {msg: 'I\'m Ready and at your service'})
 })
