4230e3c4bab15d4dd5be753012e560030f810558
Bernard Mordan <bernardmordan@gmail.com>
2014-12-23 22:28:51 +0000

    added piezo awaken tune and socket.io-client

--- comeToLife.js
+++ comeToLife.js
@@ -1,2 +1,17 @@
 var five = require('johnny-five');
 var io   = require('socket.io')();
+var Piezo = require('./Piezo');
+var socket = require('socket.io-client')('http://192.168.0.13');
+var board = new five.Board();
+
+
+board.on('ready',function(){
+  
+  var piezo = new Piezo(five);
+  piezo.awaken();
+
+  socket.on('connect', function(){
+    console.log('THREE LAWS SAFE iNodebot at your service')
+  })
+
+})
