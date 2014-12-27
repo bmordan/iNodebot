9686337ebe4746d93eea9403f17d49276cad438c
Bernard Mordan <bernardmordan@gmail.com>
2014-12-27 10:56:39 +0000

    awaken piezo routeen reinstated

--- init.js
+++ init.js
@@ -3,15 +3,13 @@ var board = new five.Board();
 var Contact = require('./Contact/socket');
 var Piezo = require('./Intent/piezo');
 var Motor = require('./Intent/motor');
-
 board.on('ready',function(){
   var contact = new Contact();
   var piezo = new Piezo(five);
   var motor = new Motor(five);
-  contact.msg('I\m ready!')
+  contact.msg('I\m ready!');
+  piezo.awaken();
   contact.socket.on('motor', function(data){
-    console.log(data.fn)
     motor[data.fn]()
   });
-  
 });
