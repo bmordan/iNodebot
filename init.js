8a09144ab508bd3e33362479d1093439a60b5bbf
bmordan <bernardmordan@gmail.com>
2014-12-28 15:30:05 +0000

    host address passed to Contact/socket

--- init.js
+++ init.js
@@ -4,12 +4,13 @@ var Contact = require('./Contact/socket');
 var Piezo = require('./Intent/piezo');
 var Motor = require('./Intent/motor');
 board.on('ready',function(){
-  var contact = new Contact();
+  var contact = new Contact('http://192.168.0.4:3000');
   var piezo = new Piezo(five);
   var motor = new Motor(five);
   contact.msg('I\m ready!');
   piezo.awaken();
   contact.socket.on('motor', function(data){
+    console.log(data.fn)
     motor[data.fn]()
   });
 });
